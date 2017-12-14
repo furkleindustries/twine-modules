555c6877b2096a954835b34f3b8aac3c4088779f
furkle <furkle@furkleindustries.com>
2017-12-14 01:56:25 -0500

    Updating build process.

--- src/exports.ts
+++ src/exports.ts
@@ -1,25 +1,39 @@
 import {
+  parse,
+} from 'css';
+import {
   parseModule,
 } from 'esprima';
 import {
   Map,
 } from 'immutable';
 import {
-  TAbstractSyntaxContent,
-} from 'twine-tree';
+  TAbstractSyntaxTree,
+} from '../node_modules/twine-tree/src/AbstractSyntaxTree/TAbstractSyntaxTree';
 
 export interface IModuleContext {
+  dontCache?: true;
   cache?: Map<string, any>;
-  passages: Array<Array<TAbstractSyntaxContent>>;
+  passages: Map<string, TAbstractSyntaxTree>;
 }
 
 export function require(passageName: string, context: IModuleContext) {
   let value;
-
   if (typeof context !== 'object' || !context) {
     throw new Error('Invalid invocation. No context was provided.');
-  } else if (typeof context.cache === 'object' && context.cache) {
+  } else if (typeof context.cache === 'object' &&
+    context.cache &&
+    passageName in context.cache &&
+    context.dontCache !== true)
+  {
     value = this.cache[passageName];
+  } else {
+    const passage = context.passages.get(passageName);
+    if (!passage) {
+      throw new Error(`No passage named ${passageName} exists.`);
+    }
+
+    value = generateValue(passage);
   }
 
   if (typeof context.passages !== 'object' || !context.passages) {
@@ -27,13 +41,16 @@ export function require(passageName: string, context: IModuleContext) {
   }
 
   if (typeof value === 'undefined') {
-    const passage = context.passages[passageName];
-    value = generateValue(passage);
+    
   }
 
   return value;
 }
 
-export function generateValue(passage: Object) {
+export function generateValue(passage: TAbstractSyntaxTree) {
+  if (!Array.isArray(passage)) {
+    throw new Error('The passage argument is not an array.');
+  }
 
+  if (passage)
 }
